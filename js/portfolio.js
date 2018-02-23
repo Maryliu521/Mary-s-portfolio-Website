5f080d0cae93f8f651104fb2db5310e401923b5e
MaryLiu <maryrthebest@gmail.com>
2018-02-23 15:30:32 -0800

    updating website

---
+++ js/portfolio.js
@@ -0,0 +1,28 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
+    document.body.style.backgroundColor = "rgba(220,220,220,0.8)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+     document.body.style.backgroundColor = "white";
+}
+
+
+(function(){
+		$(".element").typed({
+		strings: ["Software Developer"],
+		typeSpeed: 70,
+		showCursor: false,
+			  });
+		  });
+
+
+  
+
+
+
+		 
